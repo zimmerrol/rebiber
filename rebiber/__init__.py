74e9acaebc3ff346c747a588d4d562849b012ba6
zimmerrol <rzrolandzimmermann@gmail.com>
2023-05-24 10:30:42 +0200

    Modernize project configuration

--- rebiber/__init__.py
+++ rebiber/__init__.py
@@ -4,7 +4,8 @@ Rebiber: A tool for normalizing bibtex with official info.
 
 from rebiber.bib2json import load_bib_file
 from rebiber.normalize import construct_bib_db, normalize_bib
-from ._version import __version__
+
+__version__ = "1.1.3"
 
 __all__ = [
     "__version__",
